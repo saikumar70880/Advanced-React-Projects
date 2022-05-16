9f0f03a50d744f16e4e1448d37363323718aeb4d
Saikumar Aleti <kumarchintu70880@gamil.com>
2022-05-17 00:40:49 +0530

    Added a new practice project!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
+import Setup from "./tutorial/13-practice-example/";
 function App() {
   return (
     <div className="container">
---
+++ src/tutorial/13-practice-example/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <>
+      <h1>Hey, this is Saikumar Aleti!</h1>
+    </>
+  );
+};
+
+export default Title;
